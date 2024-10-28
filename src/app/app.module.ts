eb96e911678d11fb8c41c642072c2046e6f1c863
Abid Hasan <abidtkg@gmail.com>
2024-10-28 23:50:16 +0600

    update angular core 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,21 @@ import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AuthGuard } from './guards/auth.guard';
 import { TokenInterceptor } from './interceptors/token.interceptor';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    MatSnackBarModule
-  ],
-  providers: [
-    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 3500 } },
-    AuthGuard,
-    [{provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true}],
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatSnackBarModule], providers: [
+        { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 3500 } },
+        AuthGuard,
+        [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }],
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule { }
